1f8fbb7b039ba83dc4000680dbafe86c663f4dca
Vitor Martins <vitormartins.one@gmail.com>
2021-12-20 19:38:01 -0300

    construção da api seguindo os padroes RESTFULL
    retorno de dados

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes() => Ok(filmes);
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            return filme != null ? Ok(filme) : NotFound();
+        }
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O genero nao deve passar de 30 caracteres.")]
+        public string Genero { get; set; }
+        [Range(1, 30000)]
+        public int Duracao { get; set; }
+    }
+}
