80d042df13d4030c57b9f4507439c2b6ac520a10
Vitor Martins <vitormartins.one@gmail.com>
2022-03-12 21:12:49 -0300

    copia do projeto para capitulo 2

---
+++ Operações Essenciais com Verbos HTTP/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using AutoMapper;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+        {
+            Filme filme = _mapper.Map<Filme>(filmeDto);
+
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes() => Ok(_context.Filmes);
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null)
+            {
+                ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+
+                return Ok(filmeDto);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null) return NotFound();
+
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+            return NoContent(); // Padrao de retorno em Put
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            _context.Filmes.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
+ 
