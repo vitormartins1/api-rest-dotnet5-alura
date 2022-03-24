8aeb2bb50919ae802ac8674da054903809889d6f
Vitor Martins <vitormartins.one@gmail.com>
2022-03-24 00:57:51 -0300

    adicionado projeto usuarioApi para cadastro e autenticacao de usuario com identity

---
+++ FilmesAPI/UsuariosAPI/Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosAPI.Controllers
+{
+    [Route("controller")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDTO createDTO)
+        {
+
+        }
+    }
+}
---
+++ FilmesAPI/UsuariosAPI/Data/DTOs/CreateUsuarioDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosAPI.Data.DTOs
+{
+    public class CreateUsuarioDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Error password.")]
+        public string RePassword { get; set; }
+    }
+}
