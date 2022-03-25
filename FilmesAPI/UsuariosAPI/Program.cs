80d52fa0448ab59a1adab15d3982c2703cc3c3bd
Vitor Martins <vitormartins.one@gmail.com>
2022-03-25 20:22:30 -0300

    configuracao dos dados sensiveis com 'secrets'

--- FilmesAPI/UsuariosAPI/Program.cs
+++ FilmesAPI/UsuariosAPI/Program.cs
@@ -21,6 +21,9 @@ namespace UsuariosAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureAppConfiguration((context, builder) =>
+                    builder.AddUserSecrets<Program>()
+        );
     }
 }
